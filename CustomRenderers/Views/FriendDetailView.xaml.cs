d6193176e202b174caf9ee45b515c560c9e43d72
tciomenko <tciomenko@gmail.com>
2017-10-04 18:44:01 +0300

    new page FriendDetailView

---
+++ CustomRenderers/Views/FriendDetailView.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Xamarin.Forms;
+
+namespace CustomRenderers.Views
+{
+    public partial class FriendDetailView : ContentPage
+    {
+        public FriendDetailView()
+        {
+            InitializeComponent();
+        }
+    }
+}
